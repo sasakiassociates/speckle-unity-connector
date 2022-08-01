3f60d215f4fd384f4ebfcb485dcdbb316e89760d
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-08-01 15:24:01 -0400

    added client based args for main operations

---
+++ packages/core/Runtime/Args/ClientArgs.cs
@@ -0,0 +1,33 @@
+﻿namespace Speckle.ConnectorUnity.Args
+{
+	public abstract class SpeckleUnityArgs
+	{ }
+
+	public abstract class ClientArgs : SpeckleUnityArgs
+	{
+		public string message { get; set; }
+
+	}
+
+	public class ClientWorkArgs : ClientArgs
+	{
+		public bool success { get; set; }
+
+		public object client { get; set; }
+
+	}
+
+	public class ReceiveWorkArgs : ClientWorkArgs
+	{
+		public string referenceObj { get; set; }
+	}
+
+	public class SendWorkArgs : ClientWorkArgs
+	{
+
+		public string commitId { get; set; }
+		public string url { get; set; }
+
+	}
+
+}
