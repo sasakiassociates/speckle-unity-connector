3033710d137834734136915de87a67271f2a5516
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-11-28 21:31:16 -0500

    created converter manager object for data stuff

---
+++ packages/core/Runtime/Converter/ConverterManager.cs
@@ -0,0 +1,37 @@
+﻿using Speckle.ConnectorUnity.Converter;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Speckle.ConnectorUnity
+{
+
+  public class ConverterManager : ScriptableObject
+  {
+    public List<ScriptableConverter> activeConverters;
+    
+    public ConverterManager Instance { get; private set; }
+
+
+    void OnEnable()
+    {
+      Instance = this;
+    }
+
+
+
+    public static List<ScriptableConverter> GetAllConverters()
+    {
+      var items = new List<ScriptableConverter>();
+
+      return items;
+    }
+
+    public static TType GetConverter<TType>() where TType : ScriptableConverter
+    {
+      return null;
+    }
+
+  }
+
+}
